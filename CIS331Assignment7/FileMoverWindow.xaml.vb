bf43a07258cb93c94eaf2aca2c672b61685d5d92
Crimson-Traxis <trentkillinger@gmail.com>
2017-03-13 02:07:20 -0400

    First Upload

---
+++ CIS331Assignment7/FileMoverWindow.xaml.vb
@@ -0,0 +1,125 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.Windows.Threading
+
+Public Class FileMoverWindow
+
+    Private fileMover As BackgroundWorker
+    Private filesToMove As List(Of String)
+    Private newLocation As String
+    Private replaceFiles As Boolean
+    Private completedOperation As Boolean
+    Private pendingUserInput As Boolean
+
+    Public Sub New(filesToMove As List(Of String), newLocation As String, replaceFiles As Boolean)
+
+        InitializeComponent()
+
+        Me.filesToMove = filesToMove
+        Me.newLocation = newLocation
+        Me.replaceFiles = replaceFiles
+        completedOperation = False
+        pendingUserInput = False
+
+        fileMover = New BackgroundWorker()
+        fileMover.WorkerReportsProgress = True
+        fileMover.WorkerSupportsCancellation = True
+
+        AddHandler fileMover.DoWork, AddressOf FileMover_DoWork
+        AddHandler fileMover.ProgressChanged, AddressOf FileMover_ReportProgress
+        AddHandler fileMover.RunWorkerCompleted, AddressOf FileMover_WorkerCompleted
+
+    End Sub
+
+    Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
+        If Not completedOperation Then
+            Threading.Volatile.Write(pendingUserInput, True)
+            If MessageBox.Show("Exit Operation?") = MessageBoxResult.Yes Then
+                e.Cancel = True
+                fileMover.CancelAsync()
+                Me.IsEnabled = False
+            End If
+            Threading.Volatile.Write(pendingUserInput, True)
+        End If
+    End Sub
+
+    Private Sub FileMover_DoWork(sender As Object, e As DoWorkEventArgs)
+        Dim totalSize As Double = 0
+        Dim bytesProcessed As Double = 0
+        For Each file As String In filesToMove
+            Dim info As FileInfo = New FileInfo(file)
+            totalSize += info.Length
+        Next
+
+        Dispatcher.Invoke(Sub()
+                              textBlockTotalBytesToProcess.Text = totalSize
+                              textBlockTotalFiles.Text = filesToMove.Count
+                              progressBarProgress.Maximum = totalSize / 10000.0
+                          End Sub)
+
+        For index As Integer = 0 To filesToMove.Count - 1
+
+            If fileMover.CancellationPending Then
+                Exit For
+            End If
+
+            Dim fileName As String = filesToMove(index)
+            Dim info As FileInfo = New FileInfo(fileName)
+
+            Dispatcher.Invoke(Sub()
+                                  textBlockFileIndexProc.Text = index + 1
+                                  textBlockProcessingFile.Text = "Processing File: " & info.FullName
+                              End Sub)
+            If replaceFiles Then
+                Try
+                    File.Copy(info.FullName, newLocation & "\\" & info.Name, True)
+                Catch ex As Exception
+                    MessageBox.Show(ex.Message)
+                End Try
+                Dispatcher.Invoke(Sub()
+                                      fileMover.ReportProgress(info.Length)
+                                  End Sub)
+            Else
+                Dim renameInt As Integer = 1
+                If File.Exists(newLocation & "\\" & info.Name) Then
+                    While File.Exists(newLocation & "\\" & info.Name & " (" & renameInt & ")")
+                        renameInt += 1
+                    End While
+                    Try
+                        File.Copy(info.FullName, newLocation & "\\" & info.Name & " (" & renameInt & ")")
+                    Catch ex As Exception
+                        MessageBox.Show(ex.Message)
+                    End Try
+                Else
+                    Try
+                        File.Copy(info.FullName, newLocation & "\\" & info.Name, True)
+                    Catch ex As Exception
+                        MessageBox.Show(ex.Message)
+                    End Try
+                End If
+                Dispatcher.Invoke(Sub()
+                                      fileMover.ReportProgress(info.Length)
+                                  End Sub)
+            End If
+        Next
+        While Threading.Volatile.Read(pendingUserInput)
+
+        End While
+    End Sub
+
+    Private Sub FileMover_ReportProgress(sender As Object, e As ProgressChangedEventArgs)
+        Dispatcher.Invoke(Sub()
+                              Dim length As Double = e.UserState
+                              progressBarProgress.Value += length / 10000.0
+                          End Sub)
+    End Sub
+
+    Private Sub FileMover_WorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs)
+        completedOperation = True
+        Me.Close()
+    End Sub
+
+    Private Sub Window_ContentRendered(sender As Object, e As EventArgs)
+        fileMover.RunWorkerAsync()
+    End Sub
+End Class
---
+++ CIS331Assignment7/MainWindow.xaml.vb
@@ -0,0 +1,97 @@
+﻿Imports System.IO
+Imports Microsoft.Win32
+
+Class MainWindow
+
+    Private clickPoint As Point
+
+    Public Sub New()
+        InitializeComponent()
+        clickPoint = New Vector(0, 0)
+    End Sub
+
+    Private Sub sliderOverwrite_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double)) Handles sliderOverwrite.ValueChanged
+        If e.NewValue = 1 Then
+            textBlockCopyState.Text = "Overwriting Existing Files"
+        Else
+            textBlockCopyState.Text = "Not Overwriting Existing Files"
+        End If
+    End Sub
+
+    Private Sub buttonSelectSource_Click(sender As Object, e As RoutedEventArgs) Handles buttonSelectSource.Click
+        Dim folderBrowser As System.Windows.Forms.FolderBrowserDialog = New System.Windows.Forms.FolderBrowserDialog()
+        folderBrowser.Description = "Browser for a folder"
+        If folderBrowser.ShowDialog() = Forms.DialogResult.OK Then
+            textBoxSourceDir.Text = folderBrowser.SelectedPath
+        End If
+
+    End Sub
+
+    Private Sub buttonSelectDestination_Click(sender As Object, e As RoutedEventArgs) Handles buttonSelectDestination.Click
+        Dim folderBrowser As System.Windows.Forms.FolderBrowserDialog = New System.Windows.Forms.FolderBrowserDialog()
+        folderBrowser.Description = "Browser for a folder"
+        If folderBrowser.ShowDialog() = Forms.DialogResult.OK Then
+            textBoxDestinationDir.Text = folderBrowser.SelectedPath
+        End If
+    End Sub
+
+    Private Sub textBoxSourceDir_TextChanged(sender As Object, e As TextChangedEventArgs) Handles textBoxSourceDir.TextChanged
+        ReloadList(sender, listViewSourceDir)
+    End Sub
+
+    Private Sub textBoxDestinationDir_TextChanged(sender As Object, e As TextChangedEventArgs) Handles textBoxDestinationDir.TextChanged
+        ReloadList(sender, listViewDestinationDir)
+    End Sub
+
+    Private Sub listViewSourceDir_PreviewMouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles listViewSourceDir.PreviewMouseLeftButtonDown
+        clickPoint = e.GetPosition(listViewSourceDir)
+    End Sub
+
+    Private Sub listViewSourceDir_MouseMove(sender As Object, e As Input.MouseEventArgs) Handles listViewSourceDir.MouseMove
+        Dim position As Point = e.GetPosition(listViewSourceDir)
+        Dim distance As Vector = clickPoint - position
+        If e.LeftButton And
+            Math.Abs(distance.X) > SystemParameters.MinimumHorizontalDragDistance And
+                Math.Abs(distance.Y) > SystemParameters.MinimumVerticalDragDistance Then
+            Dim filesToCopy As List(Of String) = New List(Of String)
+            For Each file As String In listViewSourceDir.SelectedItems
+                filesToCopy.Add(file)
+            Next
+            Dim data As DataObject = New DataObject("filelist", filesToCopy)
+            DragDrop.DoDragDrop(listViewSourceDir, data, DragDropEffects.All)
+        End If
+    End Sub
+
+    Private Sub ReloadList(textbox As TextBox, listView As ListView)
+        listView.Items.Clear()
+        Dim filesList As List(Of String) = New List(Of String)()
+        Try
+            filesList = Directory.GetFiles(textbox.Text).ToList()
+
+        Catch ex As Exception
+
+        End Try
+        For Each file As String In filesList
+            listView.Items.Add(file)
+        Next
+    End Sub
+
+    Private Sub listViewDestinationDir_DragEnter(sender As Object, e As DragEventArgs) Handles listViewDestinationDir.DragEnter
+        If Not e.Data.GetDataPresent("filelist") Then
+            e.Effects = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub listViewDestinationDir_Drop(sender As Object, e As DragEventArgs) Handles listViewDestinationDir.Drop
+        If e.Data.GetDataPresent("filelist") Then
+            Dim filesToCopy As List(Of String) = e.Data.GetData("filelist")
+            Dim replace As Boolean = False
+            If sliderOverwrite.Value = 1 Then
+                replace = True
+            End If
+            Dim mover As FileMoverWindow = New FileMoverWindow(filesToCopy, textBoxDestinationDir.Text, replace)
+            mover.ShowDialog()
+            ReloadList(textBoxDestinationDir, listViewDestinationDir)
+        End If
+    End Sub
+End Class
